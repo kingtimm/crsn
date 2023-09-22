33c0ef62c2e04dc6b2da29f021b8f2bb51d6f765
Tim King <kingtimm@gmail.com>
2023-09-22 03:03:20 -0500

    changed layout without loading order

--- components/Faves.vue
+++ components/Faves.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { useSortable, moveArrayElement } from '@vueuse/integrations/useSortable'
+// import { useSortable, moveArrayElement } from '@vueuse/integrations/useSortable'
+import { useDraggable, type UseDraggableReturn } from 'vue-draggable-plus'
+import { useMutationObserver, useDebounceFn } from '@vueuse/core'
 import { useFaves } from '~/stores/faves';
+
 import { storeToRefs } from 'pinia'
 
 const faveStore = useFaves()
@@ -9,7 +12,6 @@ const { faves, sortState } = storeToRefs(faveStore)
 const loadingOrder = ref(true)
 
 const el = ref<HTMLElement | null>(null)
-const faveRefs = ref<HTMLElement[] | null>([])
 const st = ref([]) as Ref<Array<string>>
 
 // const sortedFavorites = computed(() => {
@@ -30,12 +32,14 @@ const st = ref([]) as Ref<Array<string>>
 
 // @ts-ignore this area isn't quite ts ready
 const sortableOptions = {
+  name: 'faves',
+  // handle: '.handle',
   store: {
     get: () => {
       loadingOrder.value = false
       return sortState.value
     },
-    set: async (sortable: any) => await faveStore.storeSortOrder(sortable.toArray()) 
+    set: async (sortable: any) => await faveStore.storeSortOrder(sortable.toArray())
   },
   // onUpdate: (e: any) => {
   //   moveArrayElement(faves, e.oldIndex, e.newIndex)
@@ -54,7 +58,28 @@ const sortableOptions = {
   //     faveStore.storeSortOrder(st.value)
   //   })
 }
+// const persist = useDebounceFn(() => {
+//   if (el.value?.children) {
+//     st.value.length = 0
+//     for (const child of el.value?.children) {
+//       for (const attribute of child.attributes) {
+//         if (attribute.name == "data-id") {
+//           st.value.push(attribute.value)
+//         }
+//       }
+//     }
+//   }
+//   console.log('new value', st.value)
+//   faveStore.storeSortOrder(st.value)
+// }, 1000)
 
+// useMutationObserver(el, (mutations) => {
+//   if (mutations[0]){
+//     persist()
+//   }
+// }, {
+//   childList: true
+// })
 
 // const ch = ref<HTMLElement | null>(null)
 
@@ -63,21 +88,20 @@ const sortableOptions = {
 // }, {immediate: true, deep: true})
 
 // @ts-ignore this area isn't quite ts ready
-const { option } = useSortable(el, faves, sortableOptions)
-
+const draggable = useDraggable(el, faves, sortableOptions)
 </script>
 
 <template>
   <UContainer>
     <h2 class="text-xl leading-10">Favorites List</h2>
 
-    <div class="inline-block" ref="el" v-show="!loadingOrder">
-      <div v-for="fave in faves" :key=fave.id :data-id="fave.id"
-        class="flex-1 flex fave gap-3 justify-between items-center mb-2 p-2 rounded bg-primary-400/10 ring-1 ring-primary-500/25">
-        <UIcon name="i-heroicons-arrows-up-down" />
-        <span class="mb-1 flex-grow text-sm">
+    <div class="inline-block" ref="el">
+      <div v-for="fave in faves" :key="fave.id" :data-id="fave.id" v-if="!loadingOrder"
+        class="flex-1 flex justify-between items-center mb-2 p-2 rounded bg-primary-400/10 ring-1 ring-primary-500/25">
+        <UIcon name="i-heroicons-arrows-up-down" class="handle" />
+        <p class="m-1 mx-2 flex-grow text-sm">
           {{ fave.firstName?.name }} {{ fave.middleName?.name }} King
-        </span>
+        </p>
         <UButton class="h-min" size="2xs" icon="i-heroicons-x-mark-20-solid" @click="faveStore.deleteFave(fave.id)" />
       </div>
     </div>
